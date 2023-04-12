4915066dda5aa3c06ab8353c011fa18312717bab
subkh4n <subkh4n@gmail.com>
2023-04-12 11:07:26 +0700

    first commit

---
+++ newsdicoding/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'article.dart';
+import 'detail_page.dart';
+import 'list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => const NewsListPage(),
+        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+              article: ModalRoute.of(context)?.settings.arguments as Article,
+            ),
+        ArticleWebView.routeName: (context) => ArticleWebView(
+              url: ModalRoute.of(context)?.settings.arguments as String,
+            ),
+      },
+    );
+  }
+}
